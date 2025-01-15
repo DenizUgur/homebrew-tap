86d85272ea11bd3bf0bbe7c0ab5716141261037b
Deniz Ugur <7467169+DenizUgur@users.noreply.github.com>
2025-01-14 21:41:51 -0800

    update dependencies

--- Formula/solarpaper.rb
+++ Formula/solarpaper.rb
@@ -1,32 +1,46 @@
 class Solarpaper < Formula
-  desc "This formula will render and set the wallpaper to current solar system state"
+  desc "This formula will render and set the wallpaper to the current solar system state"
   homepage "https://github.com/DenizUgur/solarpaper"
   url "https://github.com/DenizUgur/solarpaper.git",
       revision: "4a3ab42e8edbe1f3a4e6e838a23b9027bc0c0cc3"
   version "0.0.5"
 
-  depends_on "boost" => :build
   depends_on "cmake" => :build
-  depends_on "curl" => :build
-  depends_on "wallpaper"
+  depends_on "wallpaper" => :optional
+  depends_on "boost"
+  depends_on "curl"
 
   def install
     system "cmake", "-S", "renderer", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build", "-j"
     system "cmake", "--install", "build"
 
-    # Install the script
-    bin.install "scripts/update-wallpaper.sh"
+    if build.with? "wallpaper"
+      # Install the script
+      bin.install "scripts/update-wallpaper.sh"
+    else
+      # Add a note if `wallpaper` is not installed
+      ohai "The 'wallpaper' dependency is not installed."
+      ohai "The service will not be enabled. Install with --with-wallpaper to enable wallpaper updates."
+    end
   end
 
   service do
-    run [opt_bin / "update-wallpaper.sh"]
-    run_type :interval
-    run_at_load true
-    interval 900
+    if build.with? "wallpaper"
+      run [opt_bin / "update-wallpaper.sh"]
+      run_type :interval
+      run_at_load true
+      interval 900
+    else
+      # Service is disabled when wallpaper is not installed
+      nil
+    end
   end
 
   test do
     system "#{bin}/solarpaper"
+    if build.with? "wallpaper"
+      system "#{bin}/update-wallpaper.sh"
+    end
   end
 end
