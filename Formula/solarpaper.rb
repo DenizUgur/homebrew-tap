6668055e7bc872b3db07e237f5fb34f55b9f72ba
Deniz Ugur <7467169+DenizUgur@users.noreply.github.com>
2025-01-14 21:48:10 -0800

    make wallpaper required
    Closes #1.
    
    Signed-off-by: Deniz Uğur <7467169+DenizUgur@users.noreply.github.com>

--- Formula/solarpaper.rb
+++ Formula/solarpaper.rb
@@ -8,34 +8,26 @@ class Solarpaper < Formula
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "curl"
-  depends_on "wallpaper" => :optional
+  depends_on "wallpaper"
 
   def install
     system "cmake", "-S", "renderer", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build", "-j"
     system "cmake", "--install", "build"
 
-    if build.with? "wallpaper"
-      # Install the script
-      bin.install "scripts/update-wallpaper.sh"
-    else
-      # Add a note if `wallpaper` is not installed
-      ohai "The 'wallpaper' dependency is not installed."
-      ohai "The service will not be enabled. Install with --with-wallpaper to enable wallpaper updates."
-    end
+    # Install the script
+    bin.install "scripts/update-wallpaper.sh"
   end
 
   service do
-    if build.with? "wallpaper"
-      run [opt_bin / "update-wallpaper.sh"]
-      run_type :interval
-      run_at_load true
-      interval 900
-    end
+    run [opt_bin / "update-wallpaper.sh"]
+    run_type :interval
+    run_at_load true
+    interval 900
   end
 
   test do
     system "#{bin}/solarpaper"
-    system "#{bin}/update-wallpaper.sh" if build.with? "wallpaper"
+    system "#{bin}/update-wallpaper.sh"
   end
 end
